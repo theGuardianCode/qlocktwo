869b8fbeb800b2e8877a45d76460170b1db6eb5d
theGuardianCode <nickrichard878@gmail.com>
2022-11-25 12:24:50 +1000

    hour word now appears in the dedicated section

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ let localTime;
 const hourWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
 const minuteWords = ['five', 'ten', 'fifteen', 'twenty', 'twenty five', 'thirty', 'thirty five', 'forty', 'forty five', 'fifty', 'fifty five'];
 let wordArr = [];
-let timeString = [];
+let timeString = {};
 let remainder = 0;
 
 let mainDiv = document.getElementById("main");
@@ -11,20 +11,22 @@ let mainDiv = document.getElementById("main");
 let grid = mainDiv.innerHTML;
 
 const timeToWords = (time) => {
-    timeString = []
+    timeString = {}
 
     let simpleTime = time.split(":");
-    let hour = simpleTime[0];
-    let minute = simpleTime[1];
+    let hour = parseInt(simpleTime[0]);
+    let minute = parseInt(simpleTime[1]);
     let seconds = simpleTime[2];
 
     wordArr.push(hourWords[hour > 12 ? hour - 12 : hour - 1]);
+
+    console.log(simpleTime);
 
     let roundedMinute;
     if (minute % 5 == 0) {
         roundedMinute = minute;
     } else {
-        for (let i = 1; i < 4; i++) {
+        for (let i = 1; i < 5; i++) {
             if ((minute - i) % 5 == 0) {
                 roundedMinute = minute - i;
                 remainder = i;
@@ -32,33 +34,43 @@ const timeToWords = (time) => {
         }
     }
 
-    console.log(roundedMinute);
-
     wordArr.push(minuteWords[roundedMinute / 5 - 1]);
 
     if (roundedMinute == 0) {
-        timeString[0] = wordArr[0]
-        timeString[1] = "oclock";
+        timeString = {
+            hour: wordArr[0],
+            connector: "oclock"
+        };
     } else if (roundedMinute == 30) {
-        timeString[0] = "half"
-        timeString[1] = "past";
-        timeString[2] = wordArr[0];
+        timeString = {
+            minute: "half",
+            connector: "past",
+            hour: wordArr[0]
+        };
     } else if (roundedMinute == 5 || roundedMinute == 10 || roundedMinute == 20 || roundedMinute == 25) {
-        timeString[0] = wordArr[1];
-        timeString[1] = "past";
-        timeString[2] = wordArr[0];
+        timeString = {
+            minute: wordArr[1],
+            connector: "past",
+            hour: wordArr[0]
+        };
     } else if (roundedMinute == 15) {
-        timeString[0] = "quarter"
-        timeString[1] = "past";
-        timeString[2] = wordArr[0];
+        timeString = {
+            minute: "quarter",
+            connector: "past",
+            hour: wordArr[0]
+        };
     } else if (roundedMinute == 45) {
-        timeString[0] = "quarter"
-        timeString[1] = "to";
-        timeString[3] = hourWords[hour + 1];
+        timeString = {
+            minute: "quarter",
+            connector: "to",
+            hour: hourWords[hour]
+        };
     } else if (roundedMinute == 55 || roundedMinute == 50 || roundedMinute == 40 || roundedMinute == 25) {
-        timeString[0] = minuteWords[roundedMinute - 60];
-        timeString[1] = "to"
-        timeString[2] = hourWords[hour + 1];
+        timeString = {
+            minute: minuteWords[(60 - roundedMinute) / 5 - 1],
+            connector: "to",
+            hour: hourWords[hour]
+        };
     }
 };
 
@@ -74,17 +86,25 @@ const getTime = () => {
 
     console.log(timeString);
 
-    for (let i = 0; i < timeString.length; i++) {
-        grid = mainDiv.innerHTML;
+    for (let i in timeString) {
+        
+        if (Object.keys(timeString)[i] == "hour") {
+            grid = document.getElementById("hourblock");
+        } else {
+            grid = mainDiv.innerHTML;
+        }
 
         const upperString = timeString[i].toUpperCase();
         const newString = upperString.split("").join(" ");
 
         const index = grid.indexOf(newString);
-        console.log(`${index} -> ${grid[index]}`)
+
+        console.log(grid);
 
         let updatedGrid = grid.substring(0, index) + `<span style="color: white">` + grid.substring(index, index + newString.length) + "</span>" + grid.substring(index + newString.length);
         mainDiv.innerHTML = ' ';
         mainDiv.innerHTML = updatedGrid;
+
+        i++;
     }
 };
